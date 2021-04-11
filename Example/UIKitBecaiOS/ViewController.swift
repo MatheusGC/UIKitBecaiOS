8c7a88de59080ff3e917afe7e90f553598b9df46
Matheus Golke Cardoso <matheusgoolke@gmail.com>
2021-04-11 12:49:37 -0300

    meu commit

---
+++ Example/UIKitBecaiOS/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  UIKitBecaiOS
+//
+//  Created by henrymelo on 04/09/2021.
+//  Copyright (c) 2021 henrymelo. All rights reserved.
+//
+
+import UIKit
+import UIKitBecaiOS
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var nextButton: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+}
+
